5ca1c53c0e2ec07abf5fcb0d07d6e8aca7536298
Rahul Raushan <rahulraushan137138@gmail.com>
2026-07-18 07:58:04 +0530

    Time: 1 ms (98.04%) | Memory: 47 MB (75.64%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
         int[][] interval= Arrays.copyOf(intervals,intervals.length+1);
@@ -17,5 +17,29 @@ class Solution {
                 }
         } 
         return Arrays.copyOfRange(res,0,e+1);
+    }
+}
+*/
+
+class Solution{
+    public int[][] insert(int[][] intervals, int[] newInterval){
+        ArrayList <int[]> res = new ArrayList<>();
+        int i=0;
+        int n = intervals.length;
+        while(i<n && intervals[i][1]< newInterval[0]){
+            res.add(intervals[i]);
+            i++;
+        }
+        while(i<n && intervals[i][0]<=newInterval[1]){
+            newInterval[0]=Math.min(newInterval[0],intervals[i][0]);
+            newInterval[1]=Math.max(newInterval[1],intervals[i][1]);
+            i++;
+        }
+        res.add(newInterval);
+        while(i<n){
+            res.add(intervals[i]);
+            i++;
+        }
+        return res.toArray(new int [res.size()][2]);
     }
 }
